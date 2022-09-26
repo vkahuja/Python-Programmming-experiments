8c0049e3a62646419155b71a97743f67ad27df38
VikasAhuja-PC <vikasahuja.asm@gmail.com>
2022-09-26 18:47:21 +0530

    First Commit in new branch b1

---
+++ Program_practice/Iterating_Dictonary_Element.py
@@ -0,0 +1,8 @@
+"""" Iteration Dictionary Element """""
+my_dict = {1: 'vikas', 2: 'Ruchika', 'Gujarat': 'Gandhinagar',
+           'Maharashtra': 'Mumbai',
+           'Rajasthan': 'Jaipur',
+           'Bihar': 'Patna'}
+
+for k, v in my_dict.items():
+    print(k, ":", v)
